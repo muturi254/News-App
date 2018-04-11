70cd3fba64a8f71f5a2dedf33a862bf444682e54
muturi254 <petermuturi74@gmail.com>
2018-04-11 20:03:35 +0300

    finishing up on environment set up

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+
+# initialize bootstrap
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    # create app instance
+    app = Flask(__name__)
+
+    # app configurations
+    app.config.from_object(config_options[config_name])
+
+    # initialize bootstrap extension instance in app
+    bootstrap.init_app(app)
+
+    # register bluprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
