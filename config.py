69848302e4504b44794d995d784d8496ec6e1167
muturi254 <petermuturi74@gmail.com>
2018-04-11 19:57:02 +0300

    application configuration for all platform

---
+++ config.py
@@ -0,0 +1,21 @@
+import os 
+
+BASE_URL = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+API_KEY = os.environ.get('API_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+class Config:
+    pass
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': DevConfig,
+}
