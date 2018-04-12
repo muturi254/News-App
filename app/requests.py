448d9b6436bd4bfda384dec625d7879ab395a802
muturi254 <petermuturi74@gmail.com>
2018-04-12 11:13:43 +0300

    created functions to call top news and process the result

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,5 @@
 import urllib, json
-from . models import
+# from . models import news
 # Getting api key
 api_key = None
 # Getting the movie base url
@@ -10,3 +10,38 @@ def configure_request(app):
     global api_key, base_url
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
+
+# get top_highlights
+def get_top_news():
+    '''gets top news from news api'''
+    get_news_url = base_url.format(api_key)
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+        if get_news_response['articles']:
+            news_results_list = get_news_response['articles']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
+# precess the result into objects
+def process_results(news_list):
+    news_result = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        source = news_item.get('source')
+        title = news_item.get('title')
+        description = news_item.get('description')
+        author = news_item.get('author')
+        url = news_item.get('url')
+        urlToImage = news_item.get('urlToImage')
+        publishedAt = news_item.get('publishedAt')
+
+        # set creteria to get object
+        if id and author and urlToImage:
+            news_result.append(description)
+
+    print(news_result)
+    return news_result
