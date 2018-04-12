af8d6ba8fe038926609eda4422a3314ba41fab8d
muturi254 <petermuturi74@gmail.com>
2018-04-12 11:12:46 +0300

    changed the url to suit calling of top news

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os 
 
-BASE_URL = 'https://newsapi.org/v2/everything?q=bitcoin&apiKey={}'
+BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
 API_KEY = os.environ.get('API_KEY')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
