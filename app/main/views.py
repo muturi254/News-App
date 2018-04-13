8fdd13e918ca87938df57afdf6c821d86dd2cd0b
muturi254 <petermuturi74@gmail.com>
2018-04-13 17:02:20 +0300

    created views for news sources

--- app/main/views.py
+++ app/main/views.py
@@ -5,4 +5,8 @@ from ..requests import get_top_news
 @main.route('/')
 def index():
     top_news = get_top_news()
-    return render_template('home.html', top_news = top_news)
+    return render_template('index.html', top_news = top_news)
+
+@main.route('/sources')
+def sources():
+    return render_template('sources.html')
