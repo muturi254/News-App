009433d6f96ad5b749042020902161e069d89ca0
muturi254 <petermuturi74@gmail.com>
2018-04-13 17:01:37 +0300

    updated configuration to include different api section

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 class Config:
     BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
     API_KEY = os.environ.get('API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
 class ProdConfig(Config):
